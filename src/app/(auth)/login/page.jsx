9f80079b680ab9a8790ec9a0fde5090aed29397b
Adz1q <mikolajs2211@gmail.com>
2024-10-22 20:51:55 +0200

    Auth done!

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
-import {useState} from "react";
-import {signIn} from "next-auth/react";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
 import { useFormState } from 'react-dom';
 
 const Login = () => {
